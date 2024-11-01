26206e0a9fa7e77b6baae4d4cbf9d4f5320af046
seb-ohara <seb.ohara123@gmail.com>
2024-11-01 10:20:08 +0200

    Added the ability to toggle between colors

--- script.js
+++ script.js
@@ -1,14 +1,25 @@
 //finds the square by its ID and stores it in a variable called button
 const button = document.getElementById('but');
+//code didn't work initially because block was set to a class not ID
+const block = document.getElementById('block')
+
+
 //add an event listener that looks for a click that then runs a function
 button.addEventListener("click", changeColor)
 
-//this function changes the background color to black upon click
+/*this function changes the background color to black upon click
 function changeColor (){
-    document.body.style.backgroundColor="black"
+    block.style.backgroundColor="black"
 }
+*/
 
-
-
+// remember that === is for comparison not assignment, this function allows for the toggling between colors
+function changeColor(){
+    if (block.style.backgroundColor === 'green') {
+        block.style.backgroundColor = 'black'
+    } else {
+        block.style.backgroundColor = 'green'
+    }
+}
 
 
